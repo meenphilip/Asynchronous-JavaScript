25c1f3566d4a5e898de44a9f709929fe7b26e730
Meen Philip <meenphilip@gmail.com>
2022-09-20 14:58:07 +0300

    select DOM elements

---
+++ Ajax/Json Data/script.js
@@ -0,0 +1,6 @@
+const btnEl = document.getElementById("btn");
+const buttonEL = document.getElementById("button");
+const customerEl = document.getElementById("customer");
+const customersEl = document.getElementById("customers");
+
+
