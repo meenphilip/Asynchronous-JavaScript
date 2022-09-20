2314b54c71a0f10c004079d7c2e3f7eb42276d28
Meen Philip <meenphilip@gmail.com>
2022-09-20 16:59:46 +0300

    implement the ajax to fetch a customer

--- Ajax/Json Data/script.js
+++ Ajax/Json Data/script.js
@@ -3,4 +3,38 @@ const buttonEL = document.getElementById("button");
 const customerEl = document.getElementById("customer");
 const customersEl = document.getElementById("customers");
 
+// Add Event Listner
+btnEl.addEventListener("click", loadCustomer);
 
+function loadCustomer() {
+  // create xhr obj
+  const xhr = new XMLHttpRequest();
+  // call open
+  xhr.open("GET", "customer.json", true);
+
+  //call onload
+  xhr.onload = function () {
+    //check status === 200
+    if (this.status === 200) {
+      // parse as an Object
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+      <ul>
+      <li>ID: ${customer.id}</li>
+      <li>Name: ${customer.name}</li>
+      <li>Company: ${customer.company}</li>
+      <li>Phone: ${customer.phone}</li>
+      </ul>
+      `;
+      customerEl.innerHTML = output;
+    }
+  };
+
+  // error Handling
+  xhr.onerror = function () {
+    console.log("Error");
+  };
+  //call send
+  xhr.send();
+}
