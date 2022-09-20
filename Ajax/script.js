397ca82a7e47431d4199e8000ac74f43b3067107
Meen Philip <meenphilip@gmail.com>
2022-09-20 14:27:08 +0300

    all ajax readyState values

--- Ajax/script.js
+++ Ajax/script.js
@@ -10,8 +10,12 @@ function loadData() {
 
   xhr.open("GET", "data.txt", true);
 
+  console.log("READSTATE", xhr.readyState);
+
   // call onload()
   xhr.onload = function () {
+    // check READSTATE
+    console.log("READSTATE", xhr.readyState);
     //check status
     if (this.status === 200) {
       console.log(this.responseText);
@@ -20,4 +24,11 @@ function loadData() {
 
   //call send()
   xhr.send();
+
+  // readyState values
+  // 0: request not initialized
+  // 1: server connection established
+  // 2: request received
+  // 3: processing request
+  // 4: request finished and response is ready
 }
